115bd8fde5a1aece1336111adb38d9bc5ed5d05f
kennet <kennet.ortega@digifact.com.gt>
2022-10-21 11:36:14 -0600

    update

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error'
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'standard-jsx'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks'
+  ],
+  rules: {
+    'no-unused-vars': RULES.WARN,
+    'react/prop-types': RULES.OFF,
+    'react/react-in-jsx-scope': RULES.ERROR,
+    'react-hooks/rules-of-hooks': RULES.ERROR, // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': RULES.WARN, // Checks effect dependencies
+    'no-use-before-define': RULES.OFF
+  }
+}
---
+++ Components/Box/index.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Button, Dimensions, PixelRatio, StyleSheet, TouchableHighlight, TouchableOpacity, View } from 'react-native'
+import Animated, { useSharedValue, useAnimatedStyle, useAnimatedGestureHandler, withSpring, withTiming, Easing } from 'react-native-reanimated'
+import { PanGestureHandler } from 'react-native-gesture-handler'
+
+type Props={
+    title: string
+}
+
+const Box: React.FC<Props> = ({ title }) => {
+  const widthUI = useSharedValue(20)
+  const pressed = useSharedValue(false)
+  const active = useSharedValue('#fff')
+  const endingPosition = Dimensions.get('window').width
+  const eventHandler = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      pressed.value = true
+      ctx.startX = widthUI.value
+      active.value = '#2E317E'
+    },
+    onActive: (event, ctx) => {
+      widthUI.value = ctx.startX + event.translationX
+    },
+    onEnd: (event, ctx) => {
+      pressed.value = false
+      console.log('END ANIMATION', event)
+      if (event.translationX > 50) {
+        widthUI.value = endingPosition
+      } else {
+        active.value = '#fff'
+        widthUI.value = 20
+      }
+    }
+  })
+
+  const styleA = useAnimatedStyle(() => {
+    return {
+      width: withTiming(widthUI.value, {
+        duration: 250,
+        easing: Easing.bezier(0.47, 0, 0.745, 0.715)
+      })
+    }
+  })
+
+  const backgroundColorA = useAnimatedStyle(() => {
+    return {
+      backgroundColor: withTiming(active.value, {
+        duration: 250,
+        easing: Easing.linear
+      })
+    }
+  })
+
+  return (
+    <>
+      <PanGestureHandler onGestureEvent={eventHandler}>
+        <Animated.View style={[styles.box, styleA]} pointerEvents='box-only'>
+          <Animated.View style={[{
+            flex: 1,
+            borderRadius: 13,
+            marginRight: 50
+          }, backgroundColorA]}
+          />
+        </Animated.View>
+      </PanGestureHandler>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  box: {
+    flexDirection: 'row',
+    position: 'absolute',
+    zIndex: 2,
+    height: '100%',
+    width: 20
+  }
+})
+
+export default Box
